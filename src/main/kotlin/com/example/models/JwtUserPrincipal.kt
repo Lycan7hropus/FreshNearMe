398bdd6852731e9ee107f446f9616df7ff4a9a00
Antek Zwolski <zwo7ak@gmail.com>
2023-12-18 19:40:35 +0100

    Create JwtUserPrincipal

---
+++ src/main/kotlin/com/example/models/JwtUserPrincipal.kt
@@ -0,0 +1,16 @@
+package com.example.models
+
+import com.example.utils.Role
+import io.ktor.server.auth.*
+
+data class JwtUserPrincipal(
+    val exp: Long?,
+    val roles: List<Role>, // Z realm_access
+    val email: String?,
+    val familyName: String?, // family_name
+    val givenName: String?, // given_name
+    val preferredUsername: String?, // preferred_username
+    val name: String?,
+    val emailVerified: Boolean?, // email_verified
+    val sid: String?
+) : Principal
