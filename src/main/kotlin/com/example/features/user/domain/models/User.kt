4dab4d209aec33c14bb3eed6d444b8abaac35be8
Antek Zwolski <zwo7ak@gmail.com>
2023-12-18 20:03:25 +0100

    Add toBasicDto and toDetailedDtos inside user calss

--- src/main/kotlin/com/example/features/user/domain/models/User.kt
+++ src/main/kotlin/com/example/features/user/domain/models/User.kt
@@ -2,6 +2,8 @@ package com.example.features.user.domain.models
 
 
 import com.example.features.offer.domain.Offer
+import com.example.features.user.presentation.models.BasicUserDto
+import com.example.features.user.presentation.models.DetailedUserDto
 import com.example.utils.Role
 import kotlinx.serialization.SerialName
 import org.bson.codecs.pojo.annotations.BsonId
@@ -13,4 +15,12 @@ data class User(
     val role: Role,
     @SerialName("wish_list") val wishlist: List<Offer>,
     @SerialName("posted_offers") val postedOffers: List<Offer>,
-)
+) {
+    fun toBasicDTO(): BasicUserDto {
+        TODO("Not yet implemented")
+    }
+
+    fun toDetailedDTO(): DetailedUserDto {
+        TODO("Not yet implemented")
+    }
+}
